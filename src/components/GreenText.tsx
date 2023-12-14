cf2d7fb03286aca81c434bc0f601f23dc790ef11
Dowland Aiello <dowlandaiello@gmail.com>
2023-12-14 14:38:01 -0800

    Add link embeds.

--- src/components/GreenText.tsx
+++ src/components/GreenText.tsx
@@ -1,16 +1,30 @@
 import style from "./GreenText.module.css";
+import clickable from "./Clickable.module.css";
 
 export const GreenText = ({ children }: { children: string }) => {
-  const lines = children.split("\n").map((line, i) =>
-    line.startsWith(">") ? (
-      <p className={style.greenLine} key={i}>
-        {line}
-      </p>
-    ) : (
-      <p className={style.linef} key={i}>
-        {line}
-      </p>
-    )
-  );
+  const lineParts = children.split("\n").map((line) => line.split("https"));
+
+  const lines = lineParts.map((lineParts, i) => (
+    <div className={style.line} key={i}>
+      {lineParts.map((line, j) =>
+        line.startsWith(">") ? (
+          <p className={style.greenLine} key={`${i}_${j}`}>
+            {line}
+          </p>
+        ) : line.startsWith("://") ? (
+          <a
+            href={`https${line}`}
+            target="_blank"
+            key={`${i}_${j}`}
+            className={clickable.clickable}
+          >
+            https{line}
+          </a>
+        ) : (
+          <p key={`${i}_${j}`}>{line}</p>
+        )
+      )}
+    </div>
+  ));
   return <>{lines}</>;
 };
